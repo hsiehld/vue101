89b01990a0536a7dfcaf02bc4f55f17733312fa4
謝昇富 <hsiehld@Hsiehld-MacBook-Pro.local>
2017-06-10 15:44:01 +0800

    init jokester (use vuex) add initJokes action

---
+++ applications/jokester/src/store/actions.js
@@ -0,0 +1,12 @@
+// https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten
+// https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke
+
+import * as types from './mutation-types'
+
+export const initJokes = ({commit}) => {
+  fetch('https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten', {
+    method: 'GET'
+  })
+    .then(response => response.json())
+    .then(json => commit(types.INIT_JOKES, json))
+}
