39403013c5a3bdf9347ae42e4fd6e0a66bf13934
謝昇富 <hsiehld@Hsiehld-MacBook-Pro.local>
2017-06-20 02:17:07 +0800

    realtime loading events data

--- applications/turnout/src/components/Dashboard.vue
+++ applications/turnout/src/components/Dashboard.vue
@@ -6,13 +6,20 @@
   <hr>
   <AddEvent />
   <hr>
-  {{$store.state}}
+  <div class="col-md-12">
+    <EventItem
+      v-for="(event_item, index) in this.$store.state.events"
+      :event="event_item"
+      key="index"
+    />
+  </div>
 </div>  
 </template>
 
 <script>
-import { firebaseApp } from '../firebaseApp'
+import { firebaseApp, eventsRef } from '../firebaseApp'
 import AddEvent from './AddEvent.vue'
+import EventItem from './EventItem.vue'
 
 export default {
   methods: {
@@ -23,7 +30,18 @@ export default {
   },
 
   components: {
-    AddEvent
+    AddEvent,
+    EventItem
+  },
+
+  mounted() {
+    eventsRef.on('value', snap => {
+      let events = []
+      snap.forEach(event => {
+        events.push(event.val())
+      })
+      this.$store.dispatch('setEvents', events)
+    })
   }
 }
 </script>
